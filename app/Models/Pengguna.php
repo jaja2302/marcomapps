774e5aaadeb56414e38a0d1f1ad0bd8ce5ed5b5e
jojok <valentinojaja@gmail.com>
2024-09-19 08:43:31 +0700

    fix login filament

---
+++ app/Models/Pengguna.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Pengguna extends Authenticatable
+{
+    use HasFactory, Notifiable;
+    protected $connection = 'mysql2';
+    protected $table = 'pengguna';
+    protected $primaryKey = 'user_id';
+    public $timestamps = false;
+}
